0ad79b01d10b15fdbe863c23e3b339a0f6603d52
张文东 <837775308@qq.com>
2016-11-09 18:47:29 +0800

    张文东 one版本

---
+++ app/scripts/controllers/index.js
@@ -0,0 +1,49 @@
+//mobile菜单点击事件
+var caidan = document.getElementById("zwd_btn_menu");
+var ospan = caidan.getElementsByTagName("span");
+var pd = false;
+caidan.onclick=function(){
+		if(pd==false){
+			ospan[1].style.left = 50+"px";
+			ospan[0].style.transform = "rotate(45deg) translate3d(5px, 5px, 0)";
+			ospan[2].style.transform = "rotate(-45deg) translate3d(5px, -5px, 0)";
+			setTimeout(function(){
+				ospan[1].style.display = "none";
+			},250)
+			zwd_nav.style.display = "block";
+			pd = true
+		}else{
+			ospan[1].style.display = "block";
+			ospan[0].style.transform = "rotate(0deg) translate3d(0px, 0px, 0)";
+			ospan[2].style.transform = "rotate(0deg) translate3d(0px, 0px, 0)";
+			setTimeout(function(){
+				ospan[1].style.left = 12+"px";
+			},50)
+			zwd_nav.style.display = "none";
+			pd = false;
+		}
+		
+}
+//轮播图
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
